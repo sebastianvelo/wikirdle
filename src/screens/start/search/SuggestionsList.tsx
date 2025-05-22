68e3911b5b9bbbb5d8e09e3b18c8be7e64301127
Sebastian <sebastian.velo@mercadolibre.com>
2025-05-22 20:38:46 -0300

    Enhance SuggestionsList styling with backdrop blur and improved hover effects

--- src/screens/start/search/SuggestionsList.tsx
+++ src/screens/start/search/SuggestionsList.tsx
@@ -8,12 +8,12 @@ const SuggestionsList = (props: SuggestionsListProps) => {
   return (
     <>
       {props.showSuggestions && props.suggestions.length > 0 && (
-        <ul className="absolute z-10 mt-1 w-full bg-white border border-gray-300 rounded-lg shadow-lg max-h-60 overflow-auto dark:bg-primary-950 dark:border-primary-800">
+        <ul className="absolute backdrop-blur-2xl z-50 scrollbar w-full bg-secondary-50/90 border border-gray-300 shadow-lg max-h-60 overflow-auto dark:bg-secondary-950/90 dark:border-primary-800">
           {props.suggestions.map((title: string) => (
             <li
               key={title}
               onClick={() => props.handler(title)}
-              className="px-4 py-3 hover:bg-gray-100 cursor-pointer border-b border-gray-200 last:border-0 dark:hover:bg-primary-800 dark:border-primary-800/50 dark:text-white"
+              className="px-4 py-3 hover:bg-primary-200 cursor-pointer border-b border-gray-200 last:border-0 dark:hover:bg-primary-950/80 dark:border-primary-800/50 dark:text-white"
             >
               {title}
             </li>
