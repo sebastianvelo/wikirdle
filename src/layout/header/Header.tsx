271ac20a699f32b570980bc8274573eca993b454
Sebastian <sebastian.velo@mercadolibre.com>
2025-06-07 00:45:17 -0300

    fix: remove commented-out button group from Header component

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -11,11 +11,6 @@ const Header: React.FC<HeaderProps> = ({ showLangSelector = true }) => {
         <header className="bg-white/40 dark:bg-black/40 backdrop-blur-sm py-2 px-6 border-b border-primary-200/50 dark:border-primary-800/50 shadow-lg z-50">
             <div className="max-w-6xl mx-auto flex justify-between items-center">
                 <Brand />
-                {/*<div className="flex gap-x-4">
-                    <button className="text-primary-950 dark:text-primary-300 hover:text-white transition-colors">Rankings</button>
-                    <button className="text-primary-950 dark:text-primary-300 hover:text-white transition-colors">Perfil</button>
-                    <button className="text-primary-950 dark:text-primary-300 hover:text-white transition-colors">Ayuda</button>
-                </div>*/}
                 <div className="space-x-4 flex items-center justify-center">
                     {showLangSelector && <LanguageSelector />}
                     <ThemeToggleButton />
